82b067cf80c16a25e91d7e7b51a1c488b2bdc7e5
CaioSSilva <caiosouzasilva13650@gmail.com>
2023-11-19 15:54:50 -0300

    contact refectoring

--- src/app/Components/contact/contact.component.ts
+++ src/app/Components/contact/contact.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { faDiscord, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+import {
+  faArrowRight,
+  faEnvelope,
+  faPaperPlane,
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FontAwesomeModule],
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: ['./contact.component.css'],
 })
 export class ContactComponent {
-
+  discordIcon = faDiscord;
+  rightIcon = faArrowRight;
+  whatsIcon = faWhatsapp;
+  envelopeIcon = faEnvelope;
+  paperplaneIcon = faPaperPlane;
 }
