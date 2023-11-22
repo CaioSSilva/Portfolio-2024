8b218aa72b4a34902cd6164431f0e3fcd49fdab4
CaioSSilva <caiosouzasilva13650@gmail.com>
2023-11-22 11:55:16 -0300

    dynamic favicon update

--- src/app/Components/color/color.component.ts
+++ src/app/Components/color/color.component.ts
@@ -14,11 +14,15 @@ export class ColorComponent {
   icon = faMoon;
 
   root = <DOMTokenList>document.querySelector(':root')?.classList;
+  favicon = <HTMLLinkElement>(
+    document.querySelector("link[rel='shortcut icon']")
+  );
 
   constructor() {
     if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
       this.root.add('dark__mode');
-      this.icon = faSun
+      this.icon = faSun;
+      this.setFavicon('../../../assets/dark.ico');
     }
   }
 
@@ -27,9 +31,17 @@ export class ColorComponent {
     if (this.root.contains('dark__mode')) {
       this.root.remove('dark__mode');
       this.icon = faMoon;
+      this.setFavicon('../../../assets/white.ico');
     } else {
       this.root.add('dark__mode');
-      this.icon = faSun
+      this.icon = faSun;
+      this.setFavicon('../../../assets/dark.ico');
     }
+  }
+  setFavicon(favImg: string) {
+    let link = <HTMLLinkElement>(
+      document.querySelector('head')?.querySelector('link')
+    );
+    link.href = favImg;
   }
 }
