71881e98ae287246c391b5b616b4c035a7df52aa
CaioSSilva <caiosouzasilva13650@gmail.com>
2024-02-13 19:58:26 -0300

    animations remove

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -16,7 +16,6 @@ import {
 } from '@fortawesome/free-brands-svg-icons';
 import { faArrowDown, faPaperPlane } from '@fortawesome/free-solid-svg-icons';
 import { ColorComponent } from '../color/color.component';
-import { gsap } from 'gsap';
 import { TranslateModule } from '@ngx-translate/core';
 @Component({
   selector: 'app-home',
@@ -25,31 +24,11 @@ import { TranslateModule } from '@ngx-translate/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent implements AfterViewInit {
+export class HomeComponent {
   instaIcon = faInstagram;
   githubIcon = faGithub;
   linkedinIcon = faLinkedin;
   twitterIcon = faXTwitter;
   paperplaneIcon = faPaperPlane;
   arrowdownIcon = faArrowDown;
-
-  @ViewChildren('toBottomUp')
-  toBottomUp!: QueryList<ElementRef>;
-
-  @ViewChild('homeSocial', { static: true })
-  homeSocial!: ElementRef<HTMLDivElement>;
-
-  @ViewChild('homeImg', { static: true })
-  homeImg!: ElementRef<HTMLDivElement>;
-
-  ngAfterViewInit(): void {
-    var tl = gsap.timeline();
-
-    tl.from(this.homeSocial.nativeElement, { x: 50, opacity: 0 });
-
-    this.toBottomUp.forEach((div: ElementRef<HTMLDivElement>) => {
-      tl.from(div.nativeElement, { opacity: 0, ease: 'back', y: 500 });
-    });
-    tl.from(this.homeImg.nativeElement, { x: 200, opacity: 0 });
-  }
 }
