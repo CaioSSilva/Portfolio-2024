fb90a06a274b4bf19516e1a4bcc1fa4f671a110a
CaioSSilva <caiosouzasilva13650@gmail.com>
2024-04-14 22:57:13 -0300

    mouse animation

--- src/app/components/cursor/cursor.component.ts
+++ src/app/components/cursor/cursor.component.ts
@@ -10,14 +10,14 @@ import gsap from 'gsap';
 })
 export class CursorComponent implements OnInit {
   ngOnInit() {
-    gsap.set('.ball', { xPercent: -50, yPercent: -50 });
+    gsap.set('#ball', { xPercent: -50, yPercent: -50 });
 
-    var ball = document.querySelector('.ball');
+    var ball = <HTMLDivElement>document.querySelector('#ball');
     var pos = { x: window.innerWidth / 2, y: window.innerHeight / 2 };
     var mouse = { x: pos.x, y: pos.y };
     var speed = 0.1;
 
-    var fpms = 120 / 1000;
+    var fpms = 240 / 1000;
 
     var xSet = gsap.quickSetter(ball, 'x', 'px');
     var ySet = gsap.quickSetter(ball, 'y', 'px');
@@ -54,14 +54,28 @@ export class CursorComponent implements OnInit {
         mouse.y = pos.y = e.y;
       });
 
+      document.addEventListener('mousedown', (e) => {
+        if (e.button == 0) {
+          ball.style.transition = '.1s ease-in-out';
+          ball.classList.add('ballClick');
+        }
+      });
+
+      document.addEventListener('mouseup', (e) => {
+        if (e.button == 0) {
+          ball.classList.remove('ballClick');
+          ball.style.transition = 'none';
+        }
+      });
+
       gsap.ticker.add((time, deltaTime) => {
         var delta = deltaTime * fpms;
         var dt = 1.0 - Math.pow(1.0 - speed, delta);
 
         pos.x += (mouse.x - pos.x) * dt;
         pos.y += (mouse.y - pos.y) * dt;
-        xSet(pos.x + 2);
-        ySet(pos.y + 8);
+        xSet(pos.x + 4.5);
+        ySet(pos.y + 13);
       });
 
       showAnimation.play();
