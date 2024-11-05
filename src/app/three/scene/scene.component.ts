0105ee0ec2924464dba008e398c74e703c3b2667
caiossilva <caiossilva.ibm.com>
2024-11-04 22:04:03 -0300

    Loading

--- src/app/three/scene/scene.component.ts
+++ src/app/three/scene/scene.component.ts
@@ -1,8 +1,9 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { LoadingComponent } from '../loading/loading.component';
 @Component({
   selector: 'app-scene',
   standalone: true,
-  imports: [],
+  imports: [LoadingComponent],
   templateUrl: './scene.component.html',
   styleUrl: './scene.component.css',
 })
