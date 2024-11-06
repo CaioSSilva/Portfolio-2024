89a42aacf34e75339cf4a1da2be8178e1b6b6e64
CaioSSilva <caiosouzasilva@13650@gmail.com>
2024-11-06 02:41:21 -0300

    orbit fix

--- src/app/three/services/orbitControls.service.ts
+++ src/app/three/services/orbitControls.service.ts
@@ -1,7 +1,7 @@
 // src/app/three/orbit-controls.service.ts
 import { Injectable } from '@angular/core';
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { CameraService } from './camera.service';
 import { RendererService } from './renderer.service';
 
