8dcfc01651cc36ed8e71718c162b47432933f418
CaioSSilva <caiosouzasilva@13650@gmail.com>
2024-11-08 10:25:54 -0300

    open anim

--- src/app/three/services/raycaster.service.ts
+++ src/app/three/services/raycaster.service.ts
@@ -1,18 +1,18 @@
 import { ModelService } from './model.service';
 import { SceneComponent } from './../scene/scene.component';
-import { ElementRef, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import * as THREE from 'three';
-import { CameraService } from './camera.service';
-import { SceneService } from './scene.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class RaycasterService {
-  private mouse;
-  private raycaster;
+  private clickCount = 0;
+  private mouse: THREE.Vector2;
+  private raycaster: THREE.Raycaster;
   private mixer: THREE.AnimationMixer | null = null;
   private animationsMap: Map<string, THREE.AnimationAction> = new Map();
+
   constructor() {
     this.raycaster = new THREE.Raycaster();
     this.mouse = new THREE.Vector2();
@@ -26,35 +26,36 @@ export class RaycasterService {
     SceneComponent.getRendererContainer().nativeElement.addEventListener(
       'mousedown',
       (event) => {
-        /*this.mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-        this.mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
-
-        this.raycaster.setFromCamera(this.mouse, camera.getCamera());
-
-        const intersects = this.raycaster.intersectObjects(
-          scene.getScene().children,
-          true
-        );*/
+        this.clickCount = this.clickCount + 1;
 
         const model = ModelService.GetModelBase();
-        this.mixer = new THREE.AnimationMixer(model.scene);
+        if (!this.mixer) {
+          this.mixer = new THREE.AnimationMixer(model.scene);
 
-        model.animations.forEach((clip) => {
-          console.log(clip);
-          const action = this.mixer!.clipAction(clip);
-          this.animationsMap.set(clip.name, action);
-        });
+          model.animations.forEach((clip) => {
+            const action = this.mixer!.clipAction(clip);
+            this.animationsMap.set(clip.name, action);
+          });
+        }
 
         const animationAction = this.animationsMap.get('Animation');
-        if (animationAction) {
-          animationAction.reset().play(); // Reseta e toca a animação
+        if (!animationAction) return;
+
+        // Control the animation based on the click count
+        if (this.clickCount === 1) {
+          animationAction.reset();
+          animationAction.timeScale = 1;
+          animationAction.play();
+          animationAction.clampWhenFinished = true;
+          animationAction.loop = THREE.LoopOnce;
         }
       }
     );
   }
+
   animate() {
     if (this.mixer) {
-      this.mixer.update(0.01); // Atualiza o mixer com o delta do tempo
+      this.mixer.update(0.01); // Adjust the delta time as needed
     }
   }
 }
